d3234f45b5ad39b3372e8fe60ff1da7f851392c6
hassaNHatem <hassan.hatem.44@gmail.com>
2022-10-11 18:55:35 +0200

    styling first page

--- src/firstPage/FirstPage.js
+++ src/firstPage/FirstPage.js
@@ -1,9 +1,23 @@
+import { Link } from 'react-router-dom';
 import './first.css';
 
 function FirstPage() {
   return (
-    <div className="App">
-     <>first</>
+    <div className="first">
+   <h2>Tell us more about you</h2>
+   <div className='first-form'>
+    <input placeholder='Enter your full Name'/>
+    <input placeholder='Enter your business email' type={'email'}/>
+    <div className='country-phone'>
+        <select placeholder='country'>
+            <option>Country</option>
+        </select>
+        <input type={'number'}/>
+    </div>
+    <input type={'password'} placeholder={'Choose a password'}/>
+    <input placeholder='Repeat your password'/>
+   </div>
+   <Link to={'/scnd'} className="btn"><button>Next</button></Link>
     </div>
   );
 }
