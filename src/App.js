d556b411db684d1e4a2b7be9f41c77db77ee483c
hassaNHatem <hassan.hatem.44@gmail.com>
2022-10-12 20:20:20 +0200

    applied input validation on first page

--- src/App.js
+++ src/App.js
@@ -23,12 +23,85 @@ function App() {
   const [city , setCity] = useState('city')
   const [comanyPhone1 , setCompanyPhone1] = useState('')
   const [comanyPhone2 , setCompanyPhone2] = useState('')
+  const [password1Visable , setpassword1visable] = useState(false)
+  const [password2Visable , setpassword2visable] = useState(false)
+
+  const COUNTRIES = [ 
+    {"name": "Afghanistan", "code": "AF"}, 
+    {"name": "land Islands", "code": "AX"}, 
+    {"name": "Albania", "code": "AL"}, 
+    {"name": "Algeria", "code": "DZ"}, 
+    {"name": "American Samoa", "code": "AS"}, 
+    {"name": "AndorrA", "code": "AD"}, 
+    {"name": "Angola", "code": "AO"}, 
+    {"name": "Anguilla", "code": "AI"}, 
+    {"name": "Antarctica", "code": "AQ"}, 
+    {"name": "Antigua and Barbuda", "code": "AG"}, 
+    {"name": "Argentina", "code": "AR"}, 
+    {"name": "Armenia", "code": "AM"}, 
+    {"name": "Aruba", "code": "AW"}, 
+    {"name": "Australia", "code": "AU"}, 
+    {"name": "Austria", "code": "AT"}, 
+    {"name": "Azerbaijan", "code": "AZ"}, 
+    {"name": "Bahamas", "code": "BS"}, 
+    {"name": "Bahrain", "code": "BH"}, 
+    {"name": "Bangladesh", "code": "BD"}, 
+    {"name": "Barbados", "code": "BB"}, 
+    {"name": "Belarus", "code": "BY"}, 
+    {"name": "Belgium", "code": "BE"}, 
+    {"name": "Belize", "code": "BZ"}, 
+    {"name": "Benin", "code": "BJ"}, 
+    {"name": "Bermuda", "code": "BM"}, 
+    {"name": "Bhutan", "code": "BT"}, 
+    {"name": "Bolivia", "code": "BO"}, 
+    {"name": "Bosnia and Herzegovina", "code": "BA"}, 
+    {"name": "Botswana", "code": "BW"}, 
+    {"name": "Bouvet Island", "code": "BV"}, 
+    {"name": "Brazil", "code": "BR"}, 
+    {"name": "British Indian Ocean Territory", "code": "IO"}, 
+    {"name": "Brunei Darussalam", "code": "BN"}, 
+    {"name": "Bulgaria", "code": "BG"}, 
+    {"name": "Burkina Faso", "code": "BF"}, 
+    {"name": "Burundi", "code": "BI"}, 
+    {"name": "Cambodia", "code": "KH"}, 
+    {"name": "Cameroon", "code": "CM"}, 
+    {"name": "Canada", "code": "CA"}, 
+    {"name": "Cape Verde", "code": "CV"}, 
+    {"name": "Cayman Islands", "code": "KY"}, 
+    {"name": "Central African Republic", "code": "CF"}, 
+    {"name": "Chad", "code": "TD"}, 
+    {"name": "Chile", "code": "CL"}, 
+    {"name": "China", "code": "CN"}, 
+    {"name": "Christmas Island", "code": "CX"}, 
+    {"name": "Cocos (Keeling) Islands", "code": "CC"}, 
+    {"name": "Colombia", "code": "CO"}, 
+    {"name": "Comoros", "code": "KM"}, 
+    {"name": "Congo", "code": "CG"}, 
+    {"name": "Congo, The Democratic Republic of the", "code": "CD"}, 
+    {"name": "Cook Islands", "code": "CK"}, 
+    {"name": "Costa Rica", "code": "CR"}, 
+    {"name": "Cote DIvoire", "code": "CI"}, 
+    {"name": "Croatia", "code": "HR"}, 
+    {"name": "Cuba", "code": "CU"}, 
+    {"name": "Cyprus", "code": "CY"}, 
+    {"name": "Czech Republic", "code": "CZ"}, 
+    {"name": "Denmark", "code": "DK"}, 
+    {"name": "Djibouti", "code": "DJ"}, 
+    {"name": "Dominica", "code": "DM"}, 
+    {"name": "Dominican Republic", "code": "DO"}, 
+    {"name": "Ecuador", "code": "EC"}, 
+    {"name": "Egypt", "code": "EG"}, 
+    {"name": "El Salvador", "code": "SV"}, 
+    {"name": "Equatorial Guinea", "code": "GQ"}, 
+    {"name": "Eritrea", "code": "ER"}, 
+    {"name": "Estonia", "code": "EE"}, 
+    {"name": "Ethiopia", "code": "ET"}]
   return (
     <div className="App">
       <Router>
         <Routes>
-      <Route path="/" element={<FirstPage name={name} setName={setName} personalMail={personalMail} setPersonalMail={setPersonalMail} personalCountry={personalCountry} setPersonalCountry={setPersonalCountry} personalPhone={personalPhone} setPersonalPhone={setPersonalPhone} password={password} confrimPassword={confrimPassword}  />} />
-      <Route path="/scnd" element={ <ScndPage/>} />
+      <Route path="/" element={<FirstPage name={name} setName={setName} personalMail={personalMail} setPersonalMail={setPersonalMail} personalCountry={personalCountry} setPersonalCountry={setPersonalCountry} personalPhone={personalPhone} setPersonalPhone={setPersonalPhone} password={password} setPassword={setPassword} confrimPassword={confrimPassword} setConfirmPassword={setConfirmPassword} COUNTRIES={COUNTRIES} password1Visable={password1Visable} setpassword1visable={setpassword1visable} password2Visable={password2Visable} setpassword2visable={setpassword2visable} />} />
+      <Route path="/scnd" element={ <ScndPage  companyName={companyName} setCompanyName={setCompanyName} address={address} setAdress={setAdress} businessmail={businessmail} setBusinessMail={setBusinessMail} companyCountry={companyCountry}  setCompanyCountry={setCompanyCountry}/>}  city={city} setCity={setCity} comanyPhone1={comanyPhone1} setCompanyPhone1={setCompanyPhone1} comanyPhone2={comanyPhone2}/>
       <Route path="/confirm" element={ <Confirm/>} />
       <Route path="/congrats" element={ <Congrats/>} />
       </Routes>
--- src/firstPage/FirstPage.js
+++ src/firstPage/FirstPage.js
@@ -1,21 +1,61 @@
 import { Link } from 'react-router-dom';
 import './first.css';
-
+import PhoneInput from 'react-phone-number-input';
+import { isValidPhoneNumber } from 'react-phone-number-input'
+import 'react-phone-number-input/style.css';
+import {AiOutlineEyeInvisible , AiOutlineEye} from 'react-icons/ai'
+import validator from 'validator';
 function FirstPage(props) {
+  const pass1icon = props.password1Visable?<AiOutlineEyeInvisible/>:<AiOutlineEye/>
+  const pass2icon = props.password2Visable?<AiOutlineEyeInvisible/>:<AiOutlineEye/>
+  let validphone = isValidPhoneNumber(props.personalPhone)
+  const passwordvalidation=(pass1 ,pass2)=>{
+    let valid = false
+    if(pass1===''||pass2===''){
+      valid = false
+    }else{
+      if(pass1===pass2){
+        valid = true
+      }else{
+        valid = false
+      }
+    }
+    return valid
+  }
   return (
     <div className="first">
    <h2>Tell us more about you</h2>
    <div className='first-form'>
-    <input placeholder='Enter your full Name' value={props.name} onChange={(e)=>props.setName(e.target.value)}/>
-    <input placeholder='Enter your Personal email' type={'email'}/>
-    <div className='country-phone'>
-        <select placeholder='country'>
-            <option>Country</option>
+    <input className={props.name===''?'errorinput':''} placeholder='Enter your full Name' value={props.name} onChange={(e)=>props.setName(e.target.value)}/>
+    {props.name===''&&<span className='spanerror'>Please enter your full name</span>}
+    <input placeholder='Enter your Personal email' className={!validator.isEmail(props.personalMail)?'errorinput':''} type={'email'} value={props.personalMail} onChange={(e)=>props.setPersonalMail(e.target.value)}/>
+    {!validator.isEmail(props.personalMail)&&<span className='spanerror'>Please enter a valid email</span>}
+    <div className='country-phone' >
+      <div style={{width:'49%'}}>
+        <select placeholder='country' value={props.personalCountry} onChange={(e)=>{props.setPersonalCountry(e.target.value)}} className={props.personalCountry===''?'errorinput':''}>
+        <option value="" selected disabled hidden>Countries</option>
+        {
+        props.COUNTRIES.map((el)=>{
+          return <option value={el.code}>{el.name}</option>
+        })
+        }
         </select>
-        <input type={'number'}/>
+        {props.personalCountry===''&&<span className='spanerror'>please select a country</span>}
+        </div>
+        <div style={{width:'49%'}}>
+        <PhoneInput
+        className={validphone?'':'error'}
+      placeholder="Enter your phone number"
+      defaultCountry="EG"
+      value={props.personalPhone}
+      onChange={props.setPersonalPhone} />
+      {!validphone&&<span className='spanerror'>Enter a valid phone number</span>}
+      </div>
     </div>
-    <input type={'password'} placeholder={'Choose a password'}/>
-    <input placeholder='Repeat your password'/>
+    <div className='input-container'><input value={props.password} className={passwordvalidation(props.password , props.confrimPassword)?'':'errorinput'} onChange={(e)=>props.setPassword(e.target.value)} type={props.password1Visable?'text':'password'} placeholder={'Choose a password'}/><span onClick={()=>{props.setpassword1visable(!props.password1Visable)}}>{pass1icon}</span></div>
+    {!passwordvalidation(props.password , props.confrimPassword)&&<span className='spanerror'>Passwords are not a match or empty fields</span>}
+   <div className='input-container'> <input placeholder='Repeat your password' className={passwordvalidation(props.password , props.confrimPassword)?'':'errorinput'} value={props.confrimPassword} onChange={(e)=>props.setConfirmPassword(e.target.value)} type={props.password2Visable?'text':'password'}/><span onClick={()=>{props.setpassword2visable(!props.password2Visable)}}>{pass2icon}</span></div>
+   {!passwordvalidation(props.password , props.confrimPassword)&&<span className='spanerror'>Passwords are not a match or empty fields</span>}
    </div>
    <Link to={'/scnd'} className="btn"><button>Next</button></Link>
     </div>
